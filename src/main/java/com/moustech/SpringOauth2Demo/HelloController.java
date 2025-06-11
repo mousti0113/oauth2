608205dc6f495296bb59e77eac71ef93d6232287
Moustapha Mbaye <moustapha.mbaye@marconirovereto.it>
2025-06-11 15:41:53 +0200

    .

---
+++ src/main/java/com/moustech/SpringOauth2Demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.moustech.SpringOauth2Demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("/")
+    public String greet(){
+return "hello..I'm Mousteck";
+    }
+}
